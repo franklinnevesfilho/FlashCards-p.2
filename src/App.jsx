27c33a0b5af4d85a786fa0dd8e3fe1e481dbb1bd
franklinnevesfilho <franklin.neves.filho@gmail.com>
2023-10-03 18:51:34 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,13 @@
 import './App.css'
 import Flashcard from "./components/Flashcard.jsx";
+import cocktails from './assets/cocktails.json';
 import {useState} from "react";
+import IngredientForm from "./components/IngredientForm.jsx";
 
 function App() {
-    const cocktails = [
-        {
-            name: 'Margarita',
-            ingredients: ['Tequila', 'Triple Sec', 'Lime Juice'],
-            difficulty: 'easy'
-        },
-        {
-            name: 'Martini',
-            ingredients: ['Gin', 'Dry Vermouth'],
-            difficulty: 'hard'
-        },
-        {
-            name: 'Old Fashioned',
-            ingredients: ['Whiskey', 'Angostura Bitters', 'Sugar'],
-            difficulty: 'medium'
-        },
-        {
-            name: 'Mojito',
-            ingredients: ['White rum', 'Lime juice', 'Mint', 'Sprite / Soda Water', 'Simple Sugar'],
-            difficulty: 'easy'
-        },
-        {
-            name: 'Moscow Mule',
-            ingredients: ['Vodka', 'Lime Juice', 'Ginger Beer', 'Mint', 'Simple Sugar'],
-            difficulty: 'easy'
-        },
-        {
-            name: 'Manhattan',
-            ingredients: ['Whiskey', 'Sweet Vermouth', 'Angostura Bitters'],
-            difficulty: 'hard'
-        },
-        {
-            name: 'Daiquiri',
-            ingredients: ['White rum', 'Lime juice', 'Simple Syrup'],
-            difficulty: 'medium'
-        },
-        {
-            name: 'Whiskey Sour',
-            ingredients: ['Whiskey', 'Lime juice', 'Egg White','Simple Syrup'],
-            difficulty: 'hard'
-        },
-        {
-            name: 'Negroni',
-            ingredients: ['Gin', 'Campari', 'Sweet Vermouth'],
-            difficulty: 'medium'
-        },
-        {
-            name: 'Tom Collins',
-            ingredients: ['Gin', 'Lemon Juice', 'Simple Syrup', 'Soda Water'],
-            difficulty: 'easy'
-        },
-        {
-            name: 'Mint Julep',
-            ingredients: ['Bourbon', 'Mint', 'Simple Syrup'],
-            difficulty: 'medium'
-        },
-        {
-            name: 'White Russian',
-            ingredients: ['Vodka', 'Coffee Liqueur', 'Cream'],
-            difficulty: 'easy'
-        },
-        {
-            name: 'Bloody Mary',
-            ingredients: ['Vodka', 'Tomato Juice', 'Hot Sauce', 'Horseradish', 'Lime Juice'],
-            difficulty: 'hard'
-        },
-        {
-            name: 'Pina Colada',
-            ingredients: ['White Rum', 'Pineapple Juice', 'Coconut Cream'],
-            difficulty: 'easy'
-        },
-        {
-            name: 'Cosmopolitan',
-            ingredients: ['Vodka', 'Triple Sec', 'Lime Juice', 'Cranberry Juice'],
-            difficulty: 'hard'
-        },
-        {
-            name: 'Sazerac',
-            ingredients: ['Whiskey', 'Absinthe', 'Angostura Bitters', 'Sugar'],
-            difficulty: 'hard'
-        },
-        {
-            name: 'Sidecar',
-            ingredients: ['Cognac', 'Triple Sec', 'Lemon Juice'],
-            difficulty: 'medium'
-        },
-        {
-            name: 'French 75',
-            ingredients: ['Gin', 'Champagne', 'Lemon Juice', 'Simple Syrup'],
-            difficulty: 'medium'
-        },
-        {
-            name: 'Gimlet',
-            ingredients: ['Gin', 'Lime Juice', 'Simple Syrup'],
-            difficulty: 'medium'
-        },
-        {
-            name: 'Mimosa',
-            ingredients:['Champagne', 'Orange Juice'],
-            difficulty: 'easy'
-        }
-    ]
     const [current, setCurr] = useState(0);
     const [previous, setPrevious] = useState(0);
-    const [currentCocktail, setCocktail] = useState(cocktails[current]);
+    const [currentCocktail, setCocktail] = useState(cocktails[0]);
 
     const numCocktails = () => {
         return cocktails.length;
@@ -129,13 +29,6 @@ function App() {
 
   return (
     <>
-        <h1>Popular Cocktails</h1>
-        <h3>number of cocktails: {numCocktails()}</h3>
-        <Flashcard cocktail={currentCocktail} />
-        <div className={'btn-group'}>
-            <button className={'btn'} onClick={goBack}>Back</button>
-            <button className={'btn'} onClick={randomizer}>Next</button>
-        </div>
         <h5 className={'legend'}>Difficulty to recreate:
             <p>
                 <span className={'easy'}>EASY</span>-
@@ -143,6 +36,14 @@ function App() {
                 <span className={'hard'}>HARD</span>
             </p>
         </h5>
+        <h1>Popular Cocktails</h1>
+        <h3>number of cocktails: {numCocktails()}</h3>
+        <Flashcard cocktail={currentCocktail} />
+        <div className={'btn-group'}>
+            <button className={'btn'} onClick={goBack}>Back</button>
+            <button className={'btn'} onClick={randomizer}>Next</button>
+        </div>
+        <IngredientForm ingredients={currentCocktail.ingredients}/>
     </>
   )
 }
---
+++ src/components/IngredientForm.jsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react';
+
+const IngredientForm = ({ingredients}) => {
+    const [inputs, setInputs] = useState([]);
+    const[correct, setCorrect] = useState([]);
+
+    useEffect(() => {
+           setInputs(ingredients.reduce((acc, ingredient) => {
+               acc[ingredient] = '';
+               return acc;
+           }, {}));
+        setCorrect([]);
+
+    }, [ingredients]);
+
+    const handleChange = (e) => {
+        setInputs((prevState) => ({...prevState, [e.target.name]: e.target.value,}));
+
+        let index = ingredients.indexOf(e.target.name);
+        if ( e.target.value.toLowerCase() === ingredients[index].toLowerCase()) {
+            correct[index] = true;
+            setCorrect(correct);
+        } else {
+            correct[index] = false;
+            setCorrect(correct);
+        }
+    }
+
+    return (
+        <div >
+            <form>
+                <h4>Enter the ingredients in order</h4>
+                <div className={'input-block'}>{
+                    ingredients && ingredients.map((ingredient, i) => {
+                        return (
+                            <div key={i}>
+                                <label htmlFor={ingredient}>{i+1}.</label>
+                                <input id={ingredient} value={inputs[ingredient]} name={ingredient} className={`input ${correct[i] ? 'correct' : 'wrong'}`} type="text" onChange={handleChange}/>
+                            </div>
+                        )
+                    })}
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default IngredientForm;
